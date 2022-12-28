98fc87c495b5cc01e9232af6ed2d1efa59faa204
Safwan <sfwnisme@gmail.com>
2022-12-28 21:33:44 +0300

    .

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# scrollAnimation
+# zak.com
+[Live Demo](https://sfwnisme.github.io/zak.com/)
+.
+.
+.
+![Image](Zak.com%20Monitor%20View.png)
--- main.js
+++ main.js
@@ -11,9 +11,11 @@ function animation() {
         window.pageYOffset <
           e.offsetHeight + e.offsetTop + window.innerHeight * 0.1
       ) {
-        e.classList.add('unabel-anim-up')
+        e.style.cssText = `transform: translateY(0); opacity: 1;`;
+        e.classList.add("unabel-anim-up");
       } else {
-        e.classList.add('disabel-anim-up')
+        e.style.cssText = `transform: translateY(100px); opacity: 0;`;
+        e.classList.add("disabel-anim-up");
       }
     });
   });
@@ -29,6 +31,25 @@ function animation01() {
         e.style.cssText = `transform: translateY(0); opacity: 1;`;
       } else {
         e.style.cssText = `transform: translateY(100px); opacity: 0;`;
+      }
+    });
+  });
+}
+
+function animation02() {
+  window.addEventListener("scroll", () => {
+    ele.forEach((e) => {
+      if (
+        window.pageYOffset >=
+        e.offsetHeight + e.offsetTop - window.innerHeight * 1.2
+      ) {
+        // e.style.cssText = `transform: translateY(0); opacity: 1;`;
+        e.classList.add("unable-anim-up");
+        e.classList.remove("disable-anim-up");
+      } else {
+        // e.style.cssText = `transform: translateY(100px); opacity: 0;`;
+        e.classList.add("disable-anim-up");
+        e.classList.remove("unable-anim-up");
       }
     });
   });
