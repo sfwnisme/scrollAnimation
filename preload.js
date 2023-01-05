dcd6a2bdf32a93e967381a820812ad5906baa5fe
Safwan <sfwnisme@gmail.com>
2023-01-05 21:27:37 +0300

    .

--- preload.js
+++ preload.js
@@ -1,4 +1,3 @@
-/* =====[preloader first try]=====*/
 // export default function preload() {
 //   let now = performance.now();
 //   console.log(now);
@@ -14,36 +13,42 @@
 //   };
 // }
 
-/* =====[preloader second try]=====*/
+// export default function preload() {
+//   // let now = performance.now();
+//   let now = 5000;
+//   // let now = 5000;
+//   document.documentElement.style.setProperty("--preload_now", `${now}ms`)
+
+//   console.log(now);
+//   let hid = document.querySelector(".hide");
+//   window.addEventListener("load", () => {
+//     setInterval(() => {
+//       if (now) {
+//         hid.classList.add("hide-fade");
+//       }
+//     }, now);
+//   });
+// }
+
 export default function preload() {
-  /* =====[preloader elements]=====*/
-  let preloadBg = document.createElement("div");
-  preloadBg.className = "hide";
-  let preloadBox = document.createElement("div");
-  preloadBox.className = "preloadBox";
-
-  preloadBg.append(preloadBox);
-  document.body.append(preloadBg);
-
-  /* =====[preloader time]=====*/
-  // let now = performance.now() * 39;
-  let now = 5000;
+  // let now = performance.now();
+  // let now = 500;
+  let now;
+  if (performance.now <= 500) {
+    now = performance.now;
+  } else {
+    now = 1200;
+  }
+  // let now = 5000;
   document.documentElement.style.setProperty("--preload_now", `${now}ms`);
+
+  console.log(now);
   let hid = document.querySelector(".hide");
-
-  // console.log(now);
-  // window.addEventListener("load", () => {
-  //   setInterval(() => {
-  //     if (now) {
-  //       hid.classList.add("hide-fade");
-  //     }
-  //   }, now);
-  // });
-
-  /* =====[preloader function]=====*/
-  window.onload = () => {
-    preloadBg.classList.add("hide-fade");
-    preloadBox.classList.add("hid-box")
-
-  };
+  window.addEventListener("load", () => {
+    setInterval(() => {
+      if (now) {
+        hid.classList.add("hide-fade");
+      }
+    }, now);
+  });
 }
