696fff2d31383a1c7662dad9b79693f5ca30a245
sfwnisme <sfwnisme@gmail.com>
2023-02-07 17:10:18 +0300

    delete counter file

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 import scrollAnimation from "./scrollAnimation.js";
 scrollAnimation();
 
-import counterStation from "./counter.js";
-counterStation();
-
 import preload from "./preload.js";
 preload();
